5422d31c1161d1e1ef2c543c9e62610ace8ad36b
entegral <e2robby@gmail.com>
2019-02-26 13:19:00 -0800

    install redux and react-redux

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,11 +1,30 @@
-import TicketListReducer from '../../src/reducers/ticket-list-reducer';
+import ticketListReducer from '../../src/reducers/ticket-list-reducer';
+import { v4 } from 'uuid';
 
-describe('TicketListReducer', () => {
+describe('ticketListReducer', () => {
+
+  let action;
+  const sampleTicket = {
+    names: 'Someone and Someone Else',
+    location: 'Somewhere over the rainbow',
+    issue: 'There\'s a snake in my boot!',
+    id: v4()
+  };
 
   test('should return default state if no action is specified', () => {
-    expect(TicketListReducer({}, { type: null })).toEqual({});
+    expect(ticketListReducer( {}, {type: null} )).toEqual( {} );
   });
 
-
+  test('should add a new ticket to the current list of active tickets', () => {
+    const { names, location, issue, id } = sampleTicket;
+    action = {
+      names: names,
+      location: location,
+      issue: issue,
+      type: 'ADD_TICKET',
+      id: id
+    };
+    expect(ticketListReducer( {}, action )).toEqual( {[id] : sampleTicket} );
+  });
 
 });
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,5 +1,21 @@
 function ticketListReducer ( state = {}, action ) {
-  return state;
+  switch(action.type){
+  case 'ADD_TICKET':{
+    const { names, location, issue, id } = action;
+    let newState = Object.assign( {}, state, {
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+    return newState;
+  }
+  default:
+    return state;
+  }
+  
 }
 
 export default ticketListReducer;
